cfd15151bc4939abb5dc608d546031d81d46f5da
vendyggod <charli98@mail.ru>
2025-01-18 11:05:01 +0300

    refactor: delete unused imports and replace to new ones

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,17 @@
-import { GlobalStyles } from './styles';
-import { AppRoutes } from './AppRoutes';
-import { QueryClientProvider } from '@tanstack/react-query';
-import { queryClient } from './';
+import {GlobalStyles} from './styles';
+import {AppRoutes} from './AppRoutes';
+import {QueryClientProvider} from '@tanstack/react-query';
+import {queryClient} from './';
+import {FC} from "react";
+import {UserProvider} from "./providers/UserProvider.tsx";
 
-export const App: React.FC = () => {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <GlobalStyles />
-      <AppRoutes />
-    </QueryClientProvider>
-  );
+export const App: FC = () => {
+    return (
+        <QueryClientProvider client={queryClient}>
+            <GlobalStyles/>
+            <UserProvider>
+                <AppRoutes/>
+            </UserProvider>
+        </QueryClientProvider>
+    );
 };
