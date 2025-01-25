194d942841ba3b22b743aa7192ecb2ceadada0f1
vendyggod <charli98@mail.ru>
2025-01-25 14:43:51 +0300

    feat(cards): add card components to page

--- src/pages/Improve.tsx
+++ src/pages/Improve.tsx
@@ -1,3 +1,10 @@
-export const Improve: React.FC = () => {
-  return <div>Improve</div>;
+import {FC} from "react";
+import {CardList} from "../features/improve-cards/ui/CardList.tsx";
+import {Score} from "../widgets";
+
+export const Improve: FC = () => {
+    return <>
+        <Score/>
+        <CardList/>
+    </>
 };
