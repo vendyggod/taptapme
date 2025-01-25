170f4ce5427d3fac54be0c485bdb8a4360d5100f
vendyggod <charli98@mail.ru>
2025-01-25 14:06:15 +0300

    refactor(api): add complex getUser api to get user, user_settings and cards

--- src/services/apis/api.ts
+++ src/services/apis/api.ts
@@ -1,30 +1,20 @@
+import {supabase} from '../supabase/supabase';
 import {USER_ID} from '../../shared/consts';
 import {IUser, IUserSettings} from '../../shared/types';
-import {supabase} from '../supabase/supabase';
+import {TCards} from "../../shared/types";
 
 export const fetchUser = async (): Promise<IUser> => {
     const {data, error} = await supabase
         .from('users')
-        .select()
+        .select(`*, user_settings (*), user_cards (card_id, current_lvl, score_per_sec)`)
         .eq('id', USER_ID)
         .single();
 
-    if (error) {
-        throw new Error(error.message);
-    }
+    if (error) throw new Error(error.message);
 
+    console.log(data);
     return data;
 };
-
-export const fetchUserSettings = async (): Promise<IUserSettings> => {
-    const {data, error} = await supabase.from('user_settings').select().eq('id', USER_ID).single();
-
-    if (error) {
-        throw new Error(error.message);
-    }
-
-    return data
-}
 
 export const updateUserSettingsAPI = async (newSettings: IUserSettings): Promise<void> => {
     const {error} = await supabase
@@ -32,7 +22,14 @@ export const updateUserSettingsAPI = async (newSettings: IUserSettings): Promise
         .update(newSettings)
         .eq('id', newSettings.id);
 
-    if (error) {
-        throw new Error(error.message);
-    }
+    if (error) throw new Error(error.message);
 };
+
+export const fetchCards = async (): Promise<TCards> => {
+    const {data, error} = await supabase.from('cards').select('*');
+
+    if (error) throw new Error(error.message);
+
+    console.log(data)
+    return data
+}
