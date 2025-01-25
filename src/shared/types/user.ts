f646767af28f3978652ad45de9335377a98c8f92
vendyggod <charli98@mail.ru>
2025-01-25 14:49:37 +0300

    refactor: update imports regarding to api updates

--- src/shared/types/user.ts
+++ src/shared/types/user.ts
@@ -1,13 +1,13 @@
-export interface IUserInitialLocalState {
-    user: IUser;
-    userSettings: IUserSettings;
-    isInitialized: boolean
+export interface IUserInitialLocalState extends IUser {
+    isInitialized: boolean,
 }
 
-export interface IUser {
+export interface IUser<TSettings = IUserSettings, TCards = IUserCard> {
     id: number | null;
     nickname: string;
     avatar_url: string;
+    user_settings: TSettings;
+    user_cards: TCards;
 }
 
 export interface IUserSettings {
@@ -18,4 +18,11 @@ export interface IUserSettings {
     total_energy: number;
     current_energy: number;
     energy_per_sec: number;
+}
+
+export interface IUserCard {
+    user_id: number | null;
+    card_id: number | null;
+    current_lvl: number;
+    score_per_sec: number;
 }
