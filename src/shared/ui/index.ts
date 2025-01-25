c0e61927bab3cb3a2f7869e4e637e204817d90d9
vendyggod <charli98@mail.ru>
2025-01-25 14:45:28 +0300

    chore: update exports

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,2 +1,4 @@
-export { AppLayout } from './AppLayout/AppLayout';
-export { AvatarRounded } from './AvatarRounded/AvatarRounded';
+export {AppLayout} from './AppLayout/AppLayout';
+export {AvatarRounded} from './AvatarRounded/AvatarRounded';
+export {Header} from './Header/Header'
+export {Navigation} from './Navigation/Navigation'
