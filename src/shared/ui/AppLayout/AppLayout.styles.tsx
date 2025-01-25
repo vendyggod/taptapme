fcdce9edfefb1bb54a9014286835a532dd638b53
vendyggod <charli98@mail.ru>
2025-01-25 14:08:07 +0300

    fix: add overflow to make app scroll down

--- src/shared/ui/AppLayout/AppLayout.styles.tsx
+++ src/shared/ui/AppLayout/AppLayout.styles.tsx
@@ -13,12 +13,12 @@ export const Main = styled.main`
     height: 49rem;
     padding: 2rem 10px;
     /* background-color: #c9c9c9; */
+    overflow-y: auto;
 `;
 
 export const StyledAppLayout = styled.div`
     height: 60.5rem;
     overflow: scroll;
-    background-color: #c8c8;
     width: 34rem;
     border-radius: 16px;
     background-color: #000;
