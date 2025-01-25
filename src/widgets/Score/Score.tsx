813e817e8989d4912d3510a9a12463a370b03fed
vendyggod <charli98@mail.ru>
2025-01-25 14:47:25 +0300

    chore: update imports

--- src/widgets/Score/Score.tsx
+++ src/widgets/Score/Score.tsx
@@ -1,7 +1,7 @@
 import {FC} from 'react';
 import {StyledScore} from './Score.styles.tsx';
 import {useSelector} from "react-redux";
-import {getUserSettings} from "../../../entities/user/model/userSlice.ts";
+import {getUserSettings} from "../../entities/user/model/userSlice.ts";
 
 export const Score: FC = () => {
     const userSettings = useSelector(getUserSettings)
