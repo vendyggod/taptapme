ff825708f04d7f41cdac08aa6021c020403c2252
vendyggod <charli98@mail.ru>
2025-01-28 12:00:08 +0300

    feat(cards): create user_card interface and type for array

--- src/shared/types/card.ts
+++ src/shared/types/card.ts
@@ -1,3 +1,12 @@
+export interface IUserCard {
+    user_id: number | null;
+    card_id: number | null;
+    current_lvl: number;
+    score_per_sec: number;
+}
+
+export type TUserCards = IUserCard[];
+
 export interface ICard {
     id: number;
     title: string;
