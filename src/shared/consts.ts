dd6ff550b49a065f6e23c788d9d3f313732c88a2
vendyggod <charli98@mail.ru>
2025-01-28 12:02:50 +0300

    refactor: create media queries consts to style adaptive design

--- src/shared/consts.ts
+++ src/shared/consts.ts
@@ -1,1 +1,26 @@
+import {RuleSet} from "styled-components/dist/types";
+
 export const USER_ID: number = 1;
+
+const size = {
+    mobile: "320px",
+    tablet: "768px",
+    laptop: "1024px",
+    desktop: "2560px",
+}
+
+export const tablet = (styles: RuleSet<object>) => `
+    @media (min-width: ${size.tablet}) {
+        ${styles};
+    }
+`;
+export const desktop = (styles: RuleSet<object>) => `
+    @media (min-width: ${size.desktop}) {
+        ${styles};
+    }
+`;
+export const laptop = (styles: RuleSet<object>) => `
+    @media (min-width: ${size.laptop}) {
+        ${styles};
+    }
+`;
