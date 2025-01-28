7543abb565c2d28529166bc46b2595716c96b01f
vendyggod <charli98@mail.ru>
2025-01-28 11:55:48 +0300

    fix: styling

--- src/widgets/AccountOverview/AccountOverview.styles.tsx
+++ src/widgets/AccountOverview/AccountOverview.styles.tsx
@@ -1,10 +1,13 @@
 import styled from 'styled-components';
 
 export const StyledAccountOverview = styled.div`
-  padding: 0 10px;
-
-  display: flex;
-  align-items: center;
-  gap: 1.5rem;
-  font-size: 1.6rem; // tempo
+    padding: 0 10px;
+    display: flex;
+    align-items: center;
+    gap: 1.5rem;
+    font-size: 1.6rem; // tempo
 `;
+
+export const Nickname = styled.span`
+    font-size: 1.6rem;
+`
