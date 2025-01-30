0fce534da4448e9834da1dfb3837310e8b4e6d2a
vendyggod <charli98@mail.ru>
2025-01-30 22:40:44 +0300

    feat(cards): paste some logic from CardList props drilling

--- src/features/cards/ui/Card/Card.tsx
+++ src/features/cards/ui/Card/Card.tsx
@@ -1,5 +1,5 @@
 import {FC} from "react";
-import {ICard, IUserCard} from "../../../../shared/types";
+import {ICard} from "../../../../shared/types";
 import {
     CardEmoji,
     CardTitle,
@@ -10,16 +10,21 @@ import {
     ScorePerHourInfo,
     StyledCard
 } from "./Card.styles.tsx";
+import {useCards} from "../../model/useCards.ts";
 
 interface CardProps {
     card: ICard;
-    cardSettings?: IUserCard;
-    isDisabled: boolean;
-    onLevelUp: () => void;
 }
 
-export const Card: FC<CardProps> = ({card, cardSettings, isDisabled, onLevelUp}) => {
-    return <StyledCard $disabled={isDisabled} onClick={onLevelUp}>
+export const Card: FC<CardProps> = ({card}) => {
+    const {handleLvlUpCard, isCardActive, matchCards} = useCards()
+    const cardSettings = matchCards(card.id)
+
+    const handleClick = () => {
+        handleLvlUpCard(card.id)
+    }
+
+    return <StyledCard $disabled={!isCardActive(card.id)} onClick={handleClick}>
         <CardEmoji>🚗</CardEmoji>
         <CardTitle>{card.title}</CardTitle>
 
@@ -29,7 +34,7 @@ export const Card: FC<CardProps> = ({card, cardSettings, isDisabled, onLevelUp})
         </ScorePerHourInfo>
 
         <LvlInfo>
-            <CurrentLvl>Level {`${cardSettings?.current_lvl || 0}`}</CurrentLvl>
+            <CurrentLvl>Level {`${cardSettings?.current_lvl || '0'}`}</CurrentLvl>
             <PriceToLvlUp>2k</PriceToLvlUp>
         </LvlInfo>
     </StyledCard>
