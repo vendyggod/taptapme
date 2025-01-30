7a1612e289fa20df50be854ce874250ca97c3d8b
vendyggod <charli98@mail.ru>
2025-01-30 22:46:20 +0300

    fix: add clearInterval using useEffect

--- src/features/passive-income/model/usePassiveIncome.tsx
+++ src/features/passive-income/model/usePassiveIncome.tsx
@@ -1,4 +1,4 @@
-import {useCallback, useRef} from "react";
+import {useCallback, useEffect, useRef} from "react";
 import {useDispatch} from "react-redux";
 import {updateScorePassive} from "../../../entities/user/model/userSlice.ts";
 
@@ -6,11 +6,22 @@ export const usePassiveIncome = () => {
     const dispatch = useDispatch();
     const intervalRef = useRef<NodeJS.Timeout | null>(null);
 
-    return useCallback(() => {
+    const startPassiveIncome = useCallback(() => {
         if (intervalRef.current) return;
 
         intervalRef.current = setInterval(() => {
             dispatch(updateScorePassive());
         }, 1000);
     }, [dispatch]);
+
+    useEffect(() => {
+        return () => {
+            if (intervalRef.current) {
+                clearInterval(intervalRef.current);
+                intervalRef.current = null;
+            }
+        };
+    }, []);
+
+    return startPassiveIncome
 }
