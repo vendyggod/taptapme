1ccecc68a44d20629784878f70fb45545cf86f93
vendyggod <charli98@mail.ru>
2025-01-30 22:35:50 +0300

    fix: change state selection to prevent rerenders

--- src/widgets/AccountOverview/AccountOverview.tsx
+++ src/widgets/AccountOverview/AccountOverview.tsx
@@ -2,10 +2,11 @@ import {AvatarRounded} from '../../shared/ui';
 import {Nickname, StyledAccountOverview} from './AccountOverview.styles';
 import {FC} from "react";
 import {useSelector} from "react-redux";
-import {getUser} from "../../entities/user/model/userSlice.ts";
+import {RootState} from "../../app/store.ts";
 
 export const AccountOverview: FC = () => {
-    const {avatar_url, nickname} = useSelector(getUser)
+    const avatar_url = useSelector((state: RootState) => state.user.avatar_url)
+    const nickname = useSelector((state: RootState) => state.user.nickname)
 
     return (
         <StyledAccountOverview>
