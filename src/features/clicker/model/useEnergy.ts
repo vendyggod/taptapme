7b44383184ce48416235ba215696ef7b96148384
vendyggod <charli98@mail.ru>
2025-01-30 22:45:55 +0300

    chore: delete file

--- src/features/clicker/model/useEnergy.ts
+++
@@ -1,8 +0,0 @@
-import {useSelector} from "react-redux";
-import {getUserSettings} from "../../../entities/user/model/userSlice.ts";
-
-export const useEnergy = () => {
-    const {total_energy, current_energy} = useSelector(getUserSettings)
-
-    return {total_energy, current_energy}
-};
