31b7ab5f0a830e2d865c8050708e9d956462684d
vendyggod <charli98@mail.ru>
2025-01-30 22:38:30 +0300

    chore: get back app init to app layout

--- src/shared/ui/AppLayout/AppLayout.tsx
+++ src/shared/ui/AppLayout/AppLayout.tsx
@@ -2,19 +2,16 @@ import {Outlet} from 'react-router';
 import {Header, Navigation} from '../';
 import {Container, Main, StyledAppLayout} from './AppLayout.styles';
 import {FC} from "react";
-import {AppInitializer} from "../../../app/AppInitializer.tsx";
 
 export const AppLayout: FC = () => {
     return (
         <Container>
             <StyledAppLayout>
-                <AppInitializer>
-                    <Header/>
-                    <Main>
-                        <Outlet/>
-                    </Main>
-                    <Navigation/>
-                </AppInitializer>
+                <Header/>
+                <Main>
+                    <Outlet/>
+                </Main>
+                <Navigation/>
             </StyledAppLayout>
         </Container>
     );
