100cc74c68e0fd73d4d877de02022a7d16b3af1b
vendyggod <charli98@mail.ru>
2025-01-30 22:45:00 +0300

    feat(cards): bring in cards logic to a custom hook

--- src/features/cards/model/useCards.ts
+++ src/features/cards/model/useCards.ts
@@ -1,11 +1,60 @@
-import {useQuery} from "@tanstack/react-query";
-import {fetchCards} from "../../../services/apis/api.ts";
-import {useSelector} from "react-redux";
-import {getUserCards} from "../../../entities/user/model/userSlice.ts";
+import {useMutation, useQuery, useQueryClient} from "@tanstack/react-query";
+import {createNewCard, fetchAllCards, fetchUserCards, updateCardLevel} from "../../../services/apis/api.ts";
+import {useDispatch} from "react-redux";
+import {updateCards} from "../../../entities/user/model/userSlice.ts";
+import {useCallback, useEffect} from "react";
+import {IUserCard} from "../../../shared/types";
+import {USER_ID} from "../../../shared/consts.ts";
 
 export const useCards = () => {
-    const {data: cardsAll} = useQuery(['cards'], fetchCards);
-    const userCards = useSelector(getUserCards)
+    const queryClient = useQueryClient()
+    const dispatch = useDispatch();
+    const {data: cardsAll} = useQuery(['cards'], fetchAllCards);
+    const {data: userCards} = useQuery(['user_cards'], fetchUserCards);
 
-    return {cardsAll, userCards};
+    const {mutate: onLevelUp} = useMutation({
+        mutationFn: ({cardId, newLvl}: { cardId: number, newLvl: number }) => updateCardLevel(cardId, newLvl),
+        onSuccess: () => {
+            queryClient.invalidateQueries({queryKey: ['user_cards']});
+        },
+        onError: (error) => console.log(error)
+    })
+
+    const {mutate: createCard} = useMutation({
+        mutationFn: ({userId, cardId}: { userId: number, cardId: number }) => createNewCard(userId, cardId),
+        onSuccess: () => {
+            queryClient.invalidateQueries({queryKey: ['user_cards']})
+        },
+        onError: (error) => console.log(error)
+    })
+
+    const handleLvlUpCard = useCallback((cardId: number) => {
+        const userCard = userCards?.find((userCard: IUserCard) => userCard.card_id === cardId);
+
+        if (userCard) {
+            const newLvl = userCard.current_lvl + 1;
+            onLevelUp({cardId, newLvl});
+        } else {
+            createCard({userId: USER_ID, cardId})
+        }
+    }, [userCards, createCard, onLevelUp]);
+
+    const matchCards = useCallback((cardId: number) => {
+        return userCards?.find((userCard: IUserCard) => userCard.card_id === cardId)
+    }, [userCards]);
+
+    // Check if user has a card and it's lvl at least 1
+    const isCardActive = useCallback((cardId: number) => {
+        const userCard = userCards?.find((userCard: IUserCard) => userCard.card_id === cardId);
+        return userCard && userCard.current_lvl > 0;
+    }, [userCards]);
+
+    useEffect(() => {
+        if (userCards) {
+            dispatch(updateCards(userCards));
+        }
+    }, [userCards, dispatch]);
+
+
+    return {cardsAll, handleLvlUpCard, createCard, matchCards, isCardActive};
 }
