49725c5ad6318d71ed6b7d2ec41577f0c57614c3
vendyggod <charli98@mail.ru>
2025-01-30 22:42:39 +0300

    fix: change state selection to prevent rerenders

--- src/features/clicker/ui/Energy.tsx
+++ src/features/clicker/ui/Energy.tsx
@@ -1,9 +1,11 @@
 import {FC} from "react";
 import {Container, StyledEnergy} from "./Energy.styles.tsx";
-import {useEnergy} from "../";
+import {RootState} from "../../../app/store.ts";
+import {useSelector} from "react-redux";
 
 export const Energy: FC = () => {
-    const {total_energy, current_energy} = useEnergy()
+    const total_energy = useSelector((state: RootState) => state.user.user_settings.total_energy)
+    const current_energy = useSelector((state: RootState) => state.user.user_settings.current_energy)
 
     return <Container>
         <StyledEnergy>{current_energy} / {total_energy}</StyledEnergy>
