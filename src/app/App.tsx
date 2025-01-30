f3f775a75d1901360871a88632dc373b5d6af1ba
vendyggod <charli98@mail.ru>
2025-01-30 22:37:05 +0300

    refactor: change AppInitializer position in a dom tree

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,6 +4,7 @@ import {QueryClientProvider} from '@tanstack/react-query';
 import {queryClient} from './';
 import {FC} from "react";
 import {MainProvider} from "./providers/mainProvider.tsx";
+import {AppInitializer} from "./AppInitializer.tsx";
 
 export const App: FC = () => {
     return (
@@ -11,6 +12,7 @@ export const App: FC = () => {
             <GlobalStyles/>
             <MainProvider>
                 <AppRoutes/>
+                <AppInitializer/>
             </MainProvider>
         </QueryClientProvider>
     );
