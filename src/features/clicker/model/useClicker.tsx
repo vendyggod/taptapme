2d46e2f9c53a434023a9d24aae5b48bf81c1c216
vendyggod <charli98@mail.ru>
2025-01-30 22:45:25 +0300

    fix: replace direct api call to a useMutation

--- src/features/clicker/model/useClicker.tsx
+++ src/features/clicker/model/useClicker.tsx
@@ -1,22 +1,26 @@
 import {useDispatch, useSelector} from "react-redux";
-import {getUserSettings, updateScoreClick} from "../../../entities/user/model/userSlice.ts";
+import {updateScoreClick} from "../../../entities/user/model/userSlice.ts";
 import {useDebouncedCallback} from "../../../shared/hooks/useDebouncedCallback.tsx";
 import {updateUserSettingsAPI} from "../../../services/apis/api.ts";
-import {IUserSettings} from "../../../shared/types";
+import {RootState} from "../../../app/store.ts";
+import {useMutation} from "@tanstack/react-query";
 
 export const useClicker = () => {
     const dispatch = useDispatch()
-    const userSettings = useSelector(getUserSettings)
+    const userSettings = useSelector((state: RootState) => state.user.user_settings)
+    const {mutate: updateUserSettings} = useMutation({
+        mutationFn: () => updateUserSettingsAPI(userSettings),
+    })
 
-    const debouncedClick = useDebouncedCallback((settings: IUserSettings) => {
-        updateUserSettingsAPI(settings)
+    const debouncedClick = useDebouncedCallback(() => {
+        updateUserSettings()
     }, 500)
 
     const increaseScore = () => {
         dispatch(updateScoreClick())
-        debouncedClick(userSettings);
+        debouncedClick();
     };
 
-    return {increaseScore};
+    return increaseScore;
 }
 
